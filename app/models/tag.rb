cdfb8c5cfaa86d4dfd39fd677164b68dacda833f
Dakota <dakotakcoppage@me.com>
2021-09-18 22:56:25 -0600

    added validatitions to tag model and tests to validate the presence

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,5 @@
 class Tag < ApplicationRecord
+  validates_presence_of :name
+  
   belongs_to :post
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { should belong_to(:post) } 
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+  end
 end
