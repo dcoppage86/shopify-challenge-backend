e4e92f3f2444876f15083b88afd3f0ee5cb25a64
Dakota <dakotakcoppage@me.com>
2021-09-18 22:49:22 -0600

    wrote association and validation tests for the post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_many(:tags) }
+  end
+  # Validation test
+  describe "validations" do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:description) }
+    it { should validate_length_of(:description).is_at_most(280)}
+    it { should validate_length_of(:title).is_at_most(75)}
+  end
+  
 end
