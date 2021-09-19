2a17b38fbcad56333c716f7613c1c7d1d6fed16b
Dakota <dakotakcoppage@me.com>
2021-09-18 22:48:56 -0600

    added validations to pass written tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
+    validates_presence_of :title, :description
+    validates :title, length: { maximum: 75}
+    validates :description, length: { maximum: 280}
     has_many :tags
 end
