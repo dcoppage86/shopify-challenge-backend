d78eda27bbcd8299627d35fc8095b58f36cb2112
Dakota <dakotakcoppage@me.com>
2021-09-19 19:10:43 -0600

    finished basic stubbing of posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,32 @@
 class PostsController < ApplicationController
+    before_action :set_post, only: [:update, :destroy]
+
     def index
         @posts = Post.all
-        render json: @posts
+        render json: PostSerializer.new(@posts)
     end
 
     def create 
-
+        @post = Post.create(post_params)
+        if @post.save 
+            render json: PostSerializer.new(@post), status: :created 
+        else
+            render json: @post.errors, status: :unprocessable_entity
+        end
     end
 
-    def destroy 
+    def update
+        if @post.update_attributes(post_params)
+          render json: PostSerializer.new(@post), status: :ok
+        else
+            render json: @post.errors, status: :unprocessable_entity
+        end
+    end
+    
 
+    def destroy
+        @post.destroy 
+        head :no_content
     end
 
     private
@@ -18,7 +35,9 @@ class PostsController < ApplicationController
         params.require(:post).permit(:title, :description)
     end
     
-
+    def set_post
+        @post = Post.find(params[:id])
+    end
    
     
     
