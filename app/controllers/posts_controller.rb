2a621f7c1a7e43c468a105da6e071d73baae929b
Dakota <dakotakcoppage@me.com>
2021-09-19 18:48:17 -0600

    stubbing out basic controller actions for posts and tags controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,23 @@
 class PostsController < ApplicationController
     def index
         @posts = Post.all
+        render json: @posts
     end
+
+    def create 
+
+    end
+
+    def destroy 
+
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :description)
+    end
+    
 
    
     
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,21 @@
 class TagsController < ApplicationController
+
+    def def index
+        @tags = Tag.all
+    end
+
+    def create 
+
+    end
+
+    def destroy 
+
+    end
+
+    private
+    
+    def tag_params
+        params.require(:tag).permit(:name)
+    end
+
 end
