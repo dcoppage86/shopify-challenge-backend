be750c6e3da80eaaff289b429e5f631889f3bd39
Dakota <dakotakcoppage@me.com>
2021-09-19 19:12:17 -0600

    added tests for length validations on tags

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -7,5 +7,10 @@ RSpec.describe Tag, type: :model do
 
   describe "validations" do
     it { should validate_presence_of(:name) }
+    it do
+      is_expected.to validate_length_of(:name)
+        .is_at_least(1)
+        .is_at_most(15)
+    end
   end
 end
